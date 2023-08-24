8d7d6a1757c0a48026916a71dd92ff637bf789ef
quest82 <100534170+quest82@users.noreply.github.com>
2023-08-24 14:53:24 +0100

    made changes to html and css. Started work on Javascript

--- calc.js
+++ calc.js
@@ -1,3 +1,33 @@
-let button = document.querySelector('.btn')
+const input = document.querySelectorAll('.input')
+const computeDisplay = document.querySelector('.calc_computation')
+const resultDisplay = document.querySelector('.calc_result')
+const clear = document.querySelector('.clear_btn')
+
+let results = null;
 
 
+
+
+// Input Section
+function addToDisplay(variables){
+    computeDisplay.innerText += variables
+} // This function pushes the numbers and operators pressed on to the compute screen
+
+input.forEach(inputs => {
+    inputs.addEventListener('click', e => {
+        addToDisplay(inputs.innerText) 
+    })
+}) // This event listener takes the clicked input buttons and appends to the compute screen
+
+// Arithmetic Section
+
+    
+
+
+// Extras  Section
+
+clear.addEventListener('click', () => {
+    computeDisplay.innerText = ''
+    resultDisplay.innerText = ''
+}
+) // This event listener clears everything on the display
